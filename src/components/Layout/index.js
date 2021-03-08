3cf6f28c56a1799b9c9725490a4b4f304748bff3
Alina Gainanova <alinagaripova@MacBook-Pro-Alina.local>
2021-03-08 16:43:32 +0300

    feat: add bootstrap

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import Header from "../Header";
 import Footer from "../Footer";
 
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
 export default function Layout({children}) {
     return (
         <>
