cf6609bb198f35a57c549ca3a13c60c0edaa78df
Maria Soldatenko <Maria_Soldatenko@epam.com>
2021-03-08 14:58:23 +0300

    init: create Footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,17 +5,17 @@ export default function Footer() {
     return (
         <footer className={classes.footer}>
             <div>
-                <p>Created by</p>
-                <ul>
+                <p className={classes.created}>Created by</p>
+                <ul className={classes.developersList}>
                     <li className={classes.developer}><a href='https://github.com/alinagaripova'>Alina Garipova</a></li>
                     <li className={classes.developer}><a href='https://github.com/masolemia'>Maria Soldatenko</a></li>
                     <li className={classes.developer}><a href='https://github.com/vepsar'>Vasiliy Zhuk</a></li>
                     <li className={classes.developer}><a href='https://github.com/maksim-nemtsev'>Maksim Nemtsev</a></li>
                 </ul>
             </div>
-            <div>2021</div>
+            <div className={classes.year}>2021</div>
             <div>
-                <a href='https://rs.school/js/'><img src='././images/rs_school_js.svg' alt='RS School logo'></img></a>
+                <a href='https://rs.school/js/' className={classes.rslogo}><img src='https://rs.school/images/rs_school_js.svg' alt='RS School logo'></img></a>
             </div>
         </footer>
     )
