3a0cbec323b55f536d905b58899bb092aae64174
vepsar <vepsarvz@gmail.com>
2021-03-17 18:29:35 +0300

    fix: fix files

--- src/components/Gallery/gallery.js
+++ src/components/Gallery/gallery.js
@@ -3,7 +3,9 @@ import galleryassets from "./galleryfiles";
 
 import ReactImageGallery from "react-image-gallery";
 import "./gallery.scss";
+import { useParams } from "react-router";
 
 export default function Gallery() {
-  return <ReactImageGallery items={galleryassets}></ReactImageGallery>;
+  const { id } = useParams();
+  return <ReactImageGallery items={galleryassets[parseInt(id) - 1]}></ReactImageGallery>;
 }
--- src/components/Gallery/galleryfiles.js
+++ src/components/Gallery/galleryfiles.js
@@ -1,52 +1,407 @@
 const galleryassets = [
-  //   [
-  {
-    description: "Московский кремль",
-    original: "https://img.tourister.ru/files/1/5/7/1/5/8/3/3/original.jpg",
-    // original: "./assets/russia/kreml.jpg",
-    thumbnail: "https://img.tourister.ru/files/1/5/7/1/5/8/3/3/original.jpg",
-  },
-  {
-    description: "Красная площадь",
-    original: "https://img.tourister.ru/files/2/1/4/9/2/4/5/4/original.jpg",
-    thumbnail: "https://img.tourister.ru/files/2/1/4/9/2/4/5/4/original.jpg",
-  },
-  {
-    description: "Оружейная палата",
-    original: "https://img.tourister.ru/files/1/5/7/1/6/0/8/7/original.jpg",
-    thumbnail: "https://img.tourister.ru/files/1/5/7/1/6/0/8/7/original.jpg",
-  },
-  {
-    description: 'Телецентр "Останкино"',
-    original: "https://img.tourister.ru/files/1/5/8/3/7/4/1/3/original.jpg",
-    thumbnail: "https://img.tourister.ru/files/1/5/8/3/7/4/1/3/original.jpg",
-  },
-  {
-    description: "Большой театр",
-    original: "https://img.tourister.ru/files/1/6/9/3/0/6/3/9/original.jpg",
-    thumbnail: "https://img.tourister.ru/files/1/6/9/3/0/6/3/9/original.jpg",
-  },
-  {
-    description: "Старый Арбат",
-    original: "https://img.tourister.ru/files/1/5/8/8/0/4/7/6/original.jpg",
-    thumbnail: "https://img.tourister.ru/files/1/5/8/8/0/4/7/6/original.jpg",
-  },
-  {
-    description: "Собор Василия Блаженного",
-    original: "https://img.tourister.ru/files/2/1/2/9/1/9/0/8/original.jpg",
-    thumbnail: "https://img.tourister.ru/files/2/1/2/9/1/9/0/8/original.jpg",
-  },
-  {
-    description: "Москва-Сити",
-    original: "https://img.tourister.ru/files/1/6/5/7/5/8/8/8/original.jpg",
-    thumbnail: "https://img.tourister.ru/files/1/6/5/7/5/8/8/8/original.jpg",
-  },
-  {
-    description: "Выставка достижений народного хозяйства",
-    original: "https://img.tourister.ru/files/1/8/0/7/1/6/5/4/original.jpg",
-    thumbnail: "https://img.tourister.ru/files/1/8/0/7/1/6/5/4/original.jpg",
-  },
-  //   ],
+  [
+    {
+      description: "Московский кремль",
+      original: "https://img.tourister.ru/files/1/5/7/1/5/8/3/3/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/5/7/1/5/8/3/3/original.jpg",
+    },
+    {
+      description: "Красная площадь",
+      original: "https://img.tourister.ru/files/2/1/4/9/2/4/5/4/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/4/9/2/4/5/4/original.jpg",
+    },
+    {
+      description: "Оружейная палата",
+      original: "https://img.tourister.ru/files/1/5/7/1/6/0/8/7/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/5/7/1/6/0/8/7/original.jpg",
+    },
+    {
+      description: 'Телецентр "Останкино"',
+      original: "https://img.tourister.ru/files/1/5/8/3/7/4/1/3/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/5/8/3/7/4/1/3/original.jpg",
+    },
+    {
+      description: "Большой театр",
+      original: "https://img.tourister.ru/files/1/6/9/3/0/6/3/9/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/6/9/3/0/6/3/9/original.jpg",
+    },
+    {
+      description: "Старый Арбат",
+      original: "https://img.tourister.ru/files/1/5/8/8/0/4/7/6/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/5/8/8/0/4/7/6/original.jpg",
+    },
+    {
+      description: "Собор Василия Блаженного",
+      original: "https://img.tourister.ru/files/2/1/2/9/1/9/0/8/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/2/9/1/9/0/8/original.jpg",
+    },
+    {
+      description: "Москва-Сити",
+      original: "https://img.tourister.ru/files/1/6/5/7/5/8/8/8/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/6/5/7/5/8/8/8/original.jpg",
+    },
+    {
+      description: "Выставка достижений народного хозяйства",
+      original: "https://img.tourister.ru/files/1/8/0/7/1/6/5/4/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/8/0/7/1/6/5/4/original.jpg",
+    },
+  ],
+  [
+    {
+      description: "Эйфелева башня",
+      original: "https://img.tourister.ru/files/2/4/7/1/4/3/5/7/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/4/7/1/4/3/5/7/original.jpg",
+    },
+    {
+      description: "Лувр",
+      original: "https://img.tourister.ru/files/1/5/6/0/4/3/9/7/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/5/6/0/4/3/9/7/original.jpg",
+    },
+    {
+      description: "Собор Парижской Богоматери",
+      original: "https://img.tourister.ru/files/1/9/4/8/0/7/7/8/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/9/4/8/0/7/7/8/original.jpg",
+    },
+    {
+      description: "Триумфальная арка",
+      original: "https://img.tourister.ru/files/1/8/5/4/6/0/6/1/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/8/5/4/6/0/6/1/original.jpg",
+    },
+    {
+      description: "Елисейские поля",
+      original: "https://img.tourister.ru/files/1/8/6/4/0/4/3/8/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/8/6/4/0/4/3/8/original.jpg",
+    },
+    {
+      description: "Версальский дворец",
+      original: "https://img.tourister.ru/files/2/1/7/5/0/3/5/9/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/7/5/0/3/5/9/original.jpg",
+    },
+    {
+      description: "Мулен Руж",
+      original: "https://img.tourister.ru/files/1/8/5/1/9/0/7/4/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/8/5/1/9/0/7/4/original.jpg",
+    },
+    {
+      description: "Люксембургский сад",
+      original: "https://img.tourister.ru/files/2/1/2/9/3/6/7/7/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/2/9/3/6/7/7/original.jpg",
+    },
+    {
+      description: "Площадь согласия",
+      original: "https://img.tourister.ru/files/1/8/5/0/9/0/7/1/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/8/5/0/9/0/7/1/original.jpg",
+    },
+  ],
+  [
+    {
+      description: "Фудзияма",
+      original: "https://img.tourister.ru/files/1/5/8/3/7/9/9/5/original.jpeg",
+      thumbnail: "https://img.tourister.ru/files/1/5/8/3/7/9/9/5/original.jpeg",
+    },
+    {
+      description: "Токийский национальный музей",
+      original: "https://img.tourister.ru/files/2/4/8/7/0/2/6/6/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/4/8/7/0/2/6/6/original.jpg",
+    },
+    {
+      description: "Телебашня Tokyo Sky Tree",
+      original: "https://img.tourister.ru/files/1/6/7/1/2/3/0/7/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/6/7/1/2/3/0/7/original.jpg",
+    },
+    {
+      description: "Императорский дворец",
+      original: "https://img.tourister.ru/files/2/1/5/0/0/6/7/9/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/5/0/0/6/7/9/original.jpg",
+    },
+    {
+      description: "Храм кошки",
+      original: "https://img.tourister.ru/files/2/1/8/3/0/0/9/7/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/8/3/0/0/9/7/original.jpg",
+    },
+    {
+      description: "Радужный мост",
+      original:
+        "https://fregataero.ru/images/multithumb_thumbs/c_1000_525_16777215_00_images_japan_tokyo_rayon_odayba_tokyo_city_33.jpg",
+      thumbnail:
+        "https://fregataero.ru/images/multithumb_thumbs/c_1000_525_16777215_00_images_japan_tokyo_rayon_odayba_tokyo_city_33.jpg",
+    },
+    {
+      description: "Музей Ситамати",
+      original:
+        "https://nyc3.digitaloceanspaces.com/ur-media2/photos/images/000/028/817/89595a6542be59579f11376ad2c77d4a3b1a0a16_big.jpg?1339188966",
+      thumbnail:
+        "https://nyc3.digitaloceanspaces.com/ur-media2/photos/images/000/028/817/89595a6542be59579f11376ad2c77d4a3b1a0a16_big.jpg?1339188966",
+    },
+    {
+      description: "Святилище Мейдзи Дзингу",
+      original:
+        "https://a1.cdn.japantravel.com/photo/23001-126921/800!/tokyo-%D1%85%D1%80%D0%B0%D0%BC-%D0%BC%D0%B5%D0%B9%D0%B4%D0%B7%D0%B8-%D0%B4%D0%B7%D0%B8%D0%BD%D0%B3%D1%83-126921.jpg",
+      thumbnail:
+        "https://a1.cdn.japantravel.com/photo/23001-126921/800!/tokyo-%D1%85%D1%80%D0%B0%D0%BC-%D0%BC%D0%B5%D0%B9%D0%B4%D0%B7%D0%B8-%D0%B4%D0%B7%D0%B8%D0%BD%D0%B3%D1%83-126921.jpg",
+    },
+    {
+      description: "Театр Кабуки",
+      original: "http://pics.livejournal.com/olkomsomol/pic/0000ayr5",
+      thumbnail: "http://pics.livejournal.com/olkomsomol/pic/0000ayr5",
+    },
+  ],
+  [
+    {
+      description: "Белый дом",
+      original: "https://img.tourister.ru/files/1/5/9/8/7/8/7/7/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/5/9/8/7/8/7/7/original.jpg",
+    },
+    {
+      description: "Международный музей шпионажа",
+      original: "https://img.tourister.ru/files/2/5/1/5/1/1/7/3/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/5/1/5/1/1/7/3/original.jpg",
+    },
+    {
+      description: "Пентагон",
+      original: "https://cdni.rt.com/russian/images/2018.01/article/5a5b81a1370f2c08738b4594.jpg",
+      thumbnail: "https://cdni.rt.com/russian/images/2018.01/article/5a5b81a1370f2c08738b4594.jpg",
+    },
+    {
+      description: "Капитолий",
+      original: "https://i2.wp.com/www.hisour.com/wp-content/uploads/2017/09/US-Capitol.jpg?fit=1061%2C640&ssl=1",
+      thumbnail: "https://i2.wp.com/www.hisour.com/wp-content/uploads/2017/09/US-Capitol.jpg?fit=1061%2C640&ssl=1",
+    },
+    {
+      description: "Смитсоновский замок",
+      original: "https://img.tourister.ru/files/2/1/1/2/4/7/0/5/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/1/2/4/7/0/5/original.jpg",
+    },
+    {
+      description: "Национальный музей американской истории",
+      original: "https://rutraveller.ru/icache/place/8/274/009/82749_603x354.jpg",
+      thumbnail: "https://rutraveller.ru/icache/place/8/274/009/82749_603x354.jpg",
+    },
+    {
+      description: "Вашингтонский кафедральный собор",
+      original: "https://russiantouramerica.com/wp-content/uploads/2018/04/Kafedralnyj-Sobor-Vashingtona-free-1.png",
+      thumbnail: "https://russiantouramerica.com/wp-content/uploads/2018/04/Kafedralnyj-Sobor-Vashingtona-free-1.png",
+    },
+    {
+      description: "Монумент Вашингтона",
+      original: "https://images.vfl.ru/ii/1558385169/f87ec7d1/26602797_m.jpg",
+      thumbnail: "https://images.vfl.ru/ii/1558385169/f87ec7d1/26602797_m.jpg",
+    },
+    {
+      description: "Мемориал Линкольна",
+      original: "https://putidorogi-nn.ru/images/stories/severnaya_amerika/sha/memorial_linkolna_4.jpg",
+      thumbnail: "https://putidorogi-nn.ru/images/stories/severnaya_amerika/sha/memorial_linkolna_4.jpg",
+    },
+  ],
+  [
+    {
+      description: "Фонтан Треви",
+      original: "https://img.tourister.ru/files/1/8/2/5/2/6/1/0/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/8/2/5/2/6/1/0/original.jpg",
+    },
+    {
+      description: "Колизей",
+      original: "https://img.tourister.ru/files/1/5/5/2/2/9/2/1/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/5/5/2/2/9/2/1/original.jpg",
+    },
+    {
+      description: "Собор Святого Петра",
+      original: "https://img.tourister.ru/files/1/7/2/5/0/2/3/9/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/7/2/5/0/2/3/9/original.jpg",
+    },
+    {
+      description: "Вилла Боргезе",
+      original: "https://img.tourister.ru/files/2/1/0/3/9/7/9/0/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/0/3/9/7/9/0/original.jpg",
+    },
+    {
+      description: "Базилика Санта Мария Маджоре",
+      original: "https://img.tourister.ru/files/1/7/2/5/0/2/8/3/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/7/2/5/0/2/8/3/original.jpg",
+    },
+    {
+      description: "Пантеон",
+      original: "https://img.tourister.ru/files/1/7/2/6/8/0/2/2/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/7/2/6/8/0/2/2/original.jpg",
+    },
+    {
+      description: "Пьяцца дель Пополо",
+      original: "https://img.tourister.ru/files/2/1/1/2/5/3/7/4/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/1/2/5/3/7/4/original.jpg",
+    },
+    {
+      description: "Испанская лестница",
+      original: "https://img.tourister.ru/files/2/1/2/3/6/0/5/2/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/2/3/6/0/5/2/original.jpg",
+    },
+    {
+      description: "Площадь Навона",
+      original: "https://img.tourister.ru/files/2/1/1/3/2/4/9/7/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/1/3/2/4/9/7/original.jpg",
+    },
+  ],
+  [
+    {
+      description: "Великая Китайская стена",
+      original: "https://img.tourister.ru/files/1/5/8/3/7/0/0/3/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/5/8/3/7/0/0/3/original.jpg",
+    },
+    {
+      description: "Запретный город ",
+      original: "https://img.tourister.ru/files/2/2/4/6/0/6/4/1/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/2/4/6/0/6/4/1/original.jpg",
+    },
+    {
+      description: "Храм неба",
+      original: "https://img.tourister.ru/files/2/2/3/4/6/4/2/5/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/2/3/4/6/4/2/5/original.jpg",
+    },
+    {
+      description: "Летний дворец",
+      original: "https://img.tourister.ru/files/2/2/5/5/2/3/3/9/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/2/5/5/2/3/3/9/original.jpg",
+    },
+    {
+      description: "Площадь Тяньаньмэнь",
+      original: "https://img.tourister.ru/files/2/2/0/5/3/1/8/8/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/2/0/5/3/1/8/8/original.jpg",
+    },
+    {
+      description: "Мавзолей Мао Цзэдуна",
+      original: "https://img.tourister.ru/files/2/3/1/5/8/0/4/5/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/3/1/5/8/0/4/5/original.jpg",
+    },
+    {
+      description: "Храм Конфуция",
+      original: "https://img.tourister.ru/files/2/1/1/2/5/3/7/4/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/1/2/5/3/7/4/original.jpg",
+    },
+    {
+      description: "Гробницы династии Мин",
+      original: "https://img.tourister.ru/files/2/3/1/5/8/0/3/4/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/3/1/5/8/0/3/4/original.jpg",
+    },
+    {
+      description: "Парк Юаньминъюань",
+      original: "https://img.tourister.ru/files/2/3/4/3/3/6/5/6/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/3/4/3/3/6/5/6/original.jpg",
+    },
+  ],
+  [
+    {
+      description: "Пирамиды Гизы",
+      original: "https://img.tourister.ru/files/2/5/5/1/9/6/3/0/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/5/5/1/9/6/3/0/original.jpg",
+    },
+    {
+      description: "Долина Царей",
+      original: "https://img.tourister.ru/files/2/5/7/8/0/8/6/8/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/5/7/8/0/8/6/8/original.jpg",
+    },
+    {
+      description: "Храмы Абу-Симбел",
+      original:
+        "https://kudapoedymedia.s3.eu-west-1.amazonaws.com/wp-content/uploads/2020/08/02150729/%D0%A5%D1%80%D0%B0%D0%BC%D1%8B-%D0%90%D0%B1%D1%83-%D0%A1%D0%B8%D0%BC%D0%B1%D0%B5%D0%BB-%D0%B2-%D0%95%D0%B3%D0%B8%D0%BF%D1%82%D0%B5-6-800x445.jpg",
+      thumbnail:
+        "https://kudapoedymedia.s3.eu-west-1.amazonaws.com/wp-content/uploads/2020/08/02150729/%D0%A5%D1%80%D0%B0%D0%BC%D1%8B-%D0%90%D0%B1%D1%83-%D0%A1%D0%B8%D0%BC%D0%B1%D0%B5%D0%BB-%D0%B2-%D0%95%D0%B3%D0%B8%D0%BF%D1%82%D0%B5-6-800x445.jpg",
+    },
+    {
+      description: "Сфинск",
+      original:
+        "https://static.wikia.nocookie.net/mythological-creation/images/d/dd/%D0%A1%D1%84%D0%B8%D0%BD%D0%BA%D1%81.jpg/revision/latest?cb=20180922124958&path-prefix=ru",
+      thumbnail:
+        "https://static.wikia.nocookie.net/mythological-creation/images/d/dd/%D0%A1%D1%84%D0%B8%D0%BD%D0%BA%D1%81.jpg/revision/latest?cb=20180922124958&path-prefix=ru",
+    },
+    {
+      description: "Цветной каньон",
+      original: "https://ramzestour.com/wp-content/uploads/2019/07/foro-ekskursii-sharm-cvetnoy-kanjon.jpg",
+      thumbnail: "https://ramzestour.com/wp-content/uploads/2019/07/foro-ekskursii-sharm-cvetnoy-kanjon.jpg",
+    },
+  ],
+  [
+    {
+      description: "Национальный парк Крюгера",
+      original: "https://img.tourister.ru/files/2/4/0/8/7/1/9/5/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/4/0/8/7/1/9/5/original.jpg",
+    },
+    {
+      description: "Столовая гора",
+      original: "https://hidroad.com/upload/iblock/5c6/table_mountain_south_africa.jpg",
+      thumbnail: "https://hidroad.com/upload/iblock/5c6/table_mountain_south_africa.jpg",
+    },
+    {
+      description: "Замок Доброй Надежды",
+      original: "https://lifeglobe.net/xf/photoset/675/10.jpg",
+      thumbnail: "https://lifeglobe.net/xf/photoset/675/10.jpg",
+    },
+    {
+      description: "Мыс Кейп-Пойнт",
+      original: "https://img.tourister.ru/files/2/4/3/9/2/1/7/5/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/4/3/9/2/1/7/5/original.jpg",
+    },
+    {
+      description: "Зоопарк Йоханнесбурга",
+      original: "https://s3-eu-west-1.amazonaws.com/media.agentika.com/user/7c49ecde-ad64-482c-ae24-87e0e63701f4.jpeg",
+      thumbnail: "https://s3-eu-west-1.amazonaws.com/media.agentika.com/user/7c49ecde-ad64-482c-ae24-87e0e63701f4.jpeg",
+    },
+    {
+      description: "Национальный парк Танква Кару",
+      original: "https://img.tourister.ru/files/1/9/9/8/3/7/9/5/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/9/9/8/3/7/9/5/original.jpg",
+    },
+    {
+      description: "Аквариум Двух Океанов",
+      original: "https://media-cdn.sygictraveldata.com/media/1200x630/612664395a40232133447d33247d38313036323234313732",
+      thumbnail: "https://media-cdn.sygictraveldata.com/media/1200x630/612664395a40232133447d33247d38313036323234313732",
+    },
+  ],
+  [
+    {
+      description: "Биг-Бен",
+      original: "https://img.tourister.ru/files/1/5/4/4/2/5/9/3/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/5/4/4/2/5/9/3/original.jpg",
+    },
+    {
+      description: "Букингемский дворец",
+      original: "https://img.tourister.ru/files/1/5/4/4/5/8/2/9/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/5/4/4/5/8/2/9/original.jpg",
+    },
+    {
+      description: "Лондонский Тауэр",
+      original: "https://img.tourister.ru/files/1/5/4/4/8/8/4/7/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/5/4/4/8/8/4/7/original.jpg",
+    },
+    {
+      description: "Вестминстерское аббатство",
+      original: "https://img.tourister.ru/files/2/0/2/8/7/2/4/9/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/0/2/8/7/2/4/9/original.jpg",
+    },
+    {
+      description: "Стоунхендж",
+      original: "https://img.tourister.ru/files/1/8/1/5/1/7/3/7/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/8/1/5/1/7/3/7/original.jpg",
+    },
+    {
+      description: "Тауэрский мост",
+      original: "https://img.tourister.ru/files/1/7/6/7/4/3/2/0/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/1/7/6/7/4/3/2/0/original.jpg",
+    },
+    {
+      description: "Собор Святого Павла",
+      original: "https://img.tourister.ru/files/2/5/0/5/9/3/6/8/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/5/0/5/9/3/6/8/original.jpg",
+    },
+    {
+      description: "Трафальгарская площадь",
+      original: "https://img.tourister.ru/files/2/1/0/4/1/7/6/7/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/1/0/4/1/7/6/7/original.jpg",
+    },
+    {
+      description: "Лондонский глаз",
+      original: "https://img.tourister.ru/files/2/0/5/1/7/7/1/6/original.jpg",
+      thumbnail: "https://img.tourister.ru/files/2/0/5/1/7/7/1/6/original.jpg",
+    },
+  ],
 ];
 
 export default galleryassets;
